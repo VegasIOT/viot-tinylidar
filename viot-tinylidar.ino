c3803c1a69721d52eabe779312147ef798f0b685
VegasIOT <batsteg@gmail.com>
2018-02-23 08:25:15 -0800

    Add files via upload

---
+++ viot-tinylidar.ino
@@ -0,0 +1,501 @@
+/*
+  .______   .______    __    __   __    __          ___      __    __  .___________.  ______   .___  ___.      ___   .___________. __    ______   .__   __.
+  |   _  \  |   _  \  |  |  |  | |  |  |  |        /   \    |  |  |  | |           | /  __  \  |   \/   |     /   \  |           ||  |  /  __  \  |  \ |  |
+  |  |_)  | |  |_)  | |  |  |  | |  |__|  |       /  ^  \   |  |  |  | `---|  |----`|  |  |  | |  \  /  |    /  ^  \ `---|  |----`|  | |  |  |  | |   \|  |
+  |   _  <  |      /  |  |  |  | |   __   |      /  /_\  \  |  |  |  |     |  |     |  |  |  | |  |\/|  |   /  /_\  \    |  |     |  | |  |  |  | |  . `  |
+  |  |_)  | |  |\  \-.|  `--'  | |  |  |  |     /  _____  \ |  `--'  |     |  |     |  `--'  | |  |  |  |  /  _____  \   |  |     |  | |  `--'  | |  |\   |
+  |______/  | _| `.__| \______/  |__|  |__|    /__/     \__\ \______/      |__|      \______/  |__|  |__| /__/     \__\  |__|     |__|  \______/  |__| \__|
+  Thanks much to @corbanmailloux for providing a great framework for implementing flash/fade with HomeAssistant https://github.com/corbanmailloux/esp-mqtt-rgb-led
+  To use this code you will need the following dependancies: 
+  
+  - Support for the ESP8266 boards. 
+        - You can add it to the board manager by going to File -> Preference and pasting http://arduino.esp8266.com/stable/package_esp8266com_index.json into the Additional Board Managers URL field.
+        - Next, download the ESP8266 dependancies by going to Tools -> Board -> Board Manager and searching for ESP8266 and installing it.
+  
+  - You will also need to download the follow libraries by going to Sketch -> Include Libraries -> Manage Libraries
+      - DHT sensor library 
+      - Adafruit unified sensor
+      - PubSubClient
+      - ArduinoJSON
+    
+  UPDATE 16 MAY 2017 by Knutella - Fixed MQTT disconnects when wifi drops by moving around Reconnect and adding a software reset of MCU
+             
+  UPDATE 23 MAY 2017 - The MQTT_MAX_PACKET_SIZE parameter may not be setting appropriately do to a bug in the PubSub library. If the MQTT messages are not being transmitted as expected please you may need to change the MQTT_MAX_PACKET_SIZE parameter in "PubSubClient.h" directly.
+  2/23/18  - VIOT - Modified code to use the TinyLidar module.
+*/
+
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <PubSubClient.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+#include <Wire.h>
+
+
+
+/************ WIFI and MQTT INFORMATION (CHANGE THESE FOR YOUR SETUP) ******************/
+#define wifi_ssid "SSID" //type your WIFI information inside the quotes
+#define wifi_password "password"
+#define mqtt_server "192.168.1.1"
+#define mqtt_user "yourMQTTusername" 
+#define mqtt_password "yourMQTTpassword"
+#define mqtt_port 1883
+
+
+
+/************* MQTT TOPICS (change these topics as you wish)  **************************/
+#define light_state_topic "bruh/lidar"
+#define light_set_topic "bruh/lidar/set"
+
+const char* on_cmd = "ON";
+const char* off_cmd = "OFF";
+
+
+
+/**************************** FOR OTA **************************************************/
+#define SENSORNAME "lidar"
+#define OTApassword "YouPassword" // change this to whatever password you want to use when you upload OTA
+int OTAport = 8266;
+
+
+
+/**************************** PIN DEFINITIONS ********************************************/
+const int redPin = D1;
+const int greenPin = D2;
+const int bluePin = D3;
+#define PIRPIN    D5
+#define DHTPIN    D7
+#define DHTTYPE   DHT22
+#define LDRPIN    A0
+
+
+
+/**************************** SENSOR DEFINITIONS *******************************************/
+int counter =0;
+float ldrValue;
+int LDR;
+float calcLDR;
+float diffLDR = 25;
+int lidarValue = 0;
+int diffLidar = 25;
+float diffTEMP = 0.2;
+float tempValue;
+
+float diffHUM = 1;
+float humValue;
+
+int pirValue;
+int pirStatus;
+String motionStatus;
+
+char message_buff[100];
+
+int calibrationTime = 0;
+
+const int BUFFER_SIZE = 300;
+
+#define MQTT_MAX_PACKET_SIZE 512
+
+
+/******************************** GLOBALS for fade/flash *******************************/
+byte red = 255;
+byte green = 255;
+byte blue = 255;
+byte brightness = 255;
+
+byte realRed = 0;
+byte realGreen = 0;
+byte realBlue = 0;
+
+bool stateOn = false;
+
+bool startFade = false;
+unsigned long lastLoop = 0;
+int transitionTime = 0;
+bool inFade = false;
+int loopCount = 0;
+int stepR, stepG, stepB;
+int redVal, grnVal, bluVal;
+
+bool flash = false;
+bool startFlash = false;
+int flashLength = 0;
+unsigned long flashStartTime = 0;
+byte flashRed = red;
+byte flashGreen = green;
+byte flashBlue = blue;
+byte flashBrightness = brightness;
+
+
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+DHT dht(DHTPIN, DHTTYPE);
+
+
+
+/********************************** START SETUP*****************************************/
+void setup() {
+
+  Serial.begin(115200);
+  Wire.begin(D1, D2); // sda, scl
+  Wire.beginTransmission(0x10);
+  Wire.write(0x45);  
+  Wire.endTransmission();
+
+  pinMode(PIRPIN, INPUT);
+  pinMode(DHTPIN, INPUT);
+  pinMode(LDRPIN, INPUT);
+
+  delay(10);
+
+  ArduinoOTA.setPort(OTAport);
+
+  ArduinoOTA.setHostname(SENSORNAME);
+
+  ArduinoOTA.setPassword((const char *)OTApassword);
+
+  Serial.print("calibrating sensor ");
+  for (int i = 0; i < calibrationTime; i++) {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println("Starting Node named " + String(SENSORNAME));
+
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+
+
+  ArduinoOTA.onStart([]() {
+    Serial.println("Starting");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IPess: ");
+  Serial.println(WiFi.localIP());
+  reconnect();
+}
+
+
+
+
+/********************************** START SETUP WIFI*****************************************/
+void setup_wifi() {
+
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+
+
+/********************************** START CALLBACK*****************************************/
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.println(message);
+
+  if (!processJson(message)) {
+    return;
+  }
+
+  if (stateOn) {
+    // Update lights
+    realRed = map(red, 0, 255, 0, brightness);
+    realGreen = map(green, 0, 255, 0, brightness);
+    realBlue = map(blue, 0, 255, 0, brightness);
+  }
+  else {
+    realRed = 0;
+    realGreen = 0;
+    realBlue = 0;
+  }
+
+  startFade = true;
+  inFade = false; // Kill the current fade
+
+  sendState();
+}
+
+
+
+/********************************** START PROCESS JSON*****************************************/
+bool processJson(char* message) {
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.parseObject(message);
+
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return false;
+  }
+
+  if (root.containsKey("state")) {
+    if (strcmp(root["state"], on_cmd) == 0) {
+      stateOn = true;
+    }
+    else if (strcmp(root["state"], off_cmd) == 0) {
+      stateOn = false;
+    }
+  }
+
+  // If "flash" is included, treat RGB and brightness differently
+  if (root.containsKey("flash")) {
+    flashLength = (int)root["flash"] * 1000;
+
+    if (root.containsKey("brightness")) {
+      flashBrightness = root["brightness"];
+    }
+    else {
+      flashBrightness = brightness;
+    }
+
+    if (root.containsKey("color")) {
+      flashRed = root["color"]["r"];
+      flashGreen = root["color"]["g"];
+      flashBlue = root["color"]["b"];
+    }
+    else {
+      flashRed = red;
+      flashGreen = green;
+      flashBlue = blue;
+    }
+
+    flashRed = map(flashRed, 0, 255, 0, flashBrightness);
+    flashGreen = map(flashGreen, 0, 255, 0, flashBrightness);
+    flashBlue = map(flashBlue, 0, 255, 0, flashBrightness);
+
+    flash = true;
+    startFlash = true;
+  }
+  else { // Not flashing
+    flash = false;
+
+    if (root.containsKey("color")) {
+      red = root["color"]["r"];
+      green = root["color"]["g"];
+      blue = root["color"]["b"];
+    }
+
+    if (root.containsKey("brightness")) {
+      brightness = root["brightness"];
+    }
+
+    if (root.containsKey("transition")) {
+      transitionTime = root["transition"];
+    }
+    else {
+      transitionTime = 0;
+    }
+  }
+
+  return true;
+}
+
+
+
+/********************************** START SEND STATE*****************************************/
+void sendState() {
+  counter++;
+  tempValue=counter;
+  StaticJsonBuffer<BUFFER_SIZE> jsonBuffer;
+
+  JsonObject& root = jsonBuffer.createObject();
+
+  root["state"] = (stateOn) ? on_cmd : off_cmd;
+  JsonObject& color = root.createNestedObject("color");
+  color["r"] = red;
+  color["g"] = green;
+  color["b"] = blue;
+
+
+  root["brightness"] = brightness;
+  root["humidity"] = (String)humValue;
+  root["motion"] = (String)motionStatus;
+  root["ldr"] = (String)LDR;
+  root["temperature"] = (String)tempValue;
+  root["heatIndex"] = (String)calculateHeatIndex(humValue, tempValue);
+  root["lidar"] = (String)lidarValue;
+
+
+  char buffer[root.measureLength() + 1];
+  root.printTo(buffer, sizeof(buffer));
+  
+  if (lidarValue < 1) {
+    Serial.println("less than 1");}
+  else
+  {
+    Serial.println(buffer);
+    client.publish(light_state_topic, buffer, true);
+  }
+}
+
+
+/*
+ * Calculate Heat Index value AKA "Real Feel"
+ * NOAA heat index calculations taken from
+ * http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+ */
+float calculateHeatIndex(float humidity, float temp) {
+  float heatIndex= 0;
+  if (temp >= 80) {
+    heatIndex = -42.379 + 2.04901523*temp + 10.14333127*humidity;
+    heatIndex = heatIndex - .22475541*temp*humidity - .00683783*temp*temp;
+    heatIndex = heatIndex - .05481717*humidity*humidity + .00122874*temp*temp*humidity;
+    heatIndex = heatIndex + .00085282*temp*humidity*humidity - .00000199*temp*temp*humidity*humidity;
+  } else {
+     heatIndex = 0.5 * (temp + 61.0 + ((temp - 68.0)*1.2) + (humidity * 0.094));
+  }
+
+  if (humidity < 13 && 80 <= temp <= 112) {
+     float adjustment = ((13-humidity)/4) * sqrt((17-abs(temp-95.))/17);
+     heatIndex = heatIndex - adjustment;
+  }
+
+  return heatIndex;
+}
+
+
+/********************************** START SET COLOR *****************************************/
+void setColor(int inR, int inG, int inB) {
+  analogWrite(redPin, inR);
+  analogWrite(greenPin, inG);
+  analogWrite(bluePin, inB);
+
+  Serial.println("Setting LEDs:");
+  Serial.print("r: ");
+  Serial.print(inR);
+  Serial.print(", g: ");
+  Serial.print(inG);
+  Serial.print(", b: ");
+  Serial.println(inB);
+}
+
+
+
+/********************************** START RECONNECT*****************************************/
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect(SENSORNAME, mqtt_user, mqtt_password)) {
+      Serial.println("connected");
+      client.subscribe(light_set_topic);
+      setColor(0, 0, 0);
+      sendState();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+
+
+/********************************** START CHECK SENSOR **********************************/
+bool checkBoundSensor(float newValue, float prevValue, float maxDiff) {
+  return newValue < prevValue - maxDiff || newValue > prevValue + maxDiff;
+}
+
+
+/********************************** START MAIN LOOP***************************************/
+void loop() {
+
+  ArduinoOTA.handle();
+  
+  if (!client.connected()) {
+    // reconnect();
+    software_Reset();
+  }
+  client.loop();
+
+
+  
+    delay(42);
+    
+    Wire.beginTransmission(0x10);
+    
+    Wire.write("D");  
+    Wire.endTransmission();
+    //receive measurement data
+    Wire.requestFrom(0x10, 2);   
+    int16_t newLidar = ( Wire.read()<<8 | Wire.read() ); 
+    if (newLidar < 0)
+      newLidar=lidarValue;
+    int newdiff=abs(lidarValue-newLidar);
+    
+    //print it out
+    //Serial.print(F("Distance (mm) = "));
+    //Serial.print("newlidar ");   
+    //Serial.println(newLidar);
+  
+    //if (checkBoundSensor(newdiff, lidarValue, diffLidar)) {
+    if (newdiff > diffLidar) {
+      Serial.print("newdiff ");
+      Serial.println (newdiff);
+      Serial.print("lidarValue ");
+      Serial.println(lidarValue);
+      Serial.print("newLidar ");
+      Serial.println(newLidar);
+      
+      humValue=newdiff;
+      lidarValue = newLidar;
+      sendState();
+    }
+
+}
+
+
+
+/****reset***/
+void software_Reset() // Restarts program from beginning but does not reset the peripherals and registers
+{
+Serial.print("resetting");
+ESP.reset(); 
+}
+
